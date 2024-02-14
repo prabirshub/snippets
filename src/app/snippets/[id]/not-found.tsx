5dcea95ec3b0cd44dc2e21933ede2a6342895844
prabirshub <prabirsinghdigital@gmail.com>
2024-02-14 11:35:39 +0530

    project on prisma and nextjs

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SinnpetNotFound() {
+  return (
+    <div>
+      <h1 className='text-xl h-screen font-bold grid place-items-center'>
+        Sorry, but we could not find that particular snippet!
+      </h1>
+    </div>
+  )
+}
